ef48025842b31ee82ec056353e7e6cb2726329b4
anusha1107 <anushabaskaran11@gmail.com>
2025-10-23 20:56:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,10 @@ app.use(cors({
 app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.send("Server is running successfully!");
-});
 
-app.post("/submit", (req, res) => {
+
+app.post("/", (req, res) => {
   const { name, message } = req.body;
-
   console.log(`Form submitted by ${name}`);
   res.send(`Hi ${name}, your message '${message}' was received successfully!`);
 });
@@ -26,4 +23,5 @@ app.post("/submit", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
